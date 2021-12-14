dbf0bdb1b2ddd0466cdb86c3acbc542addb74b05
LapisRaider <angielimenqi7@gmail.com>
2021-12-15 00:39:22 +0800

    more dialogue stuff

--- LosingIsWinning/Assets/Scripts/Dialogue/DialogueManager.cs
+++ LosingIsWinning/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -8,36 +8,96 @@ public class DialogueManager : MonoBehaviour
     [Header("Dialogue UI")]
     public GameObject m_dialogueBox;
     public TextMeshProUGUI m_text;
+    public float m_textSpeed = 0.1f;
 
-    DialogueData m_currDialogue;
+    List<Dialogue> m_currDialogues;
 
-    int m_currDialogueOption = 0;
-    int m_currSentence = 0;
+    Dialogue m_currDiaOption;
+    DialogueText m_currText;
+
+    int m_currOptionIndex = 0;
+    int m_currSentenceIndex = 0;
     int m_currCharIndex = 0;
 
     public void Start()
     {
-        m_currDialogue = null;
+        m_currDialogues = null;
         ResetDialogue();
     }
 
     public void ResetDialogue()
     {
-        m_currDialogueOption = 0;
-        m_currSentence = 0;
+        m_currDialogues = null;
+
+        m_currDiaOption = null;
+        m_currText = null;
+
+        m_currOptionIndex = 0;
+        m_currSentenceIndex = 0;
         m_currCharIndex = 0;
+    }
+
+    public void StartDialogue(DialogueData newDialogue)
+    {
+        ResetDialogue();
+
+        m_currDialogues = newDialogue.m_Dialogues;
+        m_currDiaOption = m_currDialogues[0];
+        m_currText = m_currDiaOption.m_dialogue[0];
     }
 
     public void NextSentence()
     {
-        //check if end, if yes close the UI
+        StopCoroutine(PrintText(null));
 
-        //check if sentence is done printing out and no options out yet
-            //start next sentence printing
-            //or put out the options 
-            
-
-        //not done
+        if (m_currCharIndex < m_currText.m_text.Length)
+        {
             //print out the entire sentence
+            m_currCharIndex = m_currText.m_text.Length;
+            m_text.text = m_currText.m_text;
+        }
+        else if (m_currCharIndex >= m_currDiaOption.m_dialogue.Count) //check if end
+        {
+            //or put out the options
+            if (m_currDiaOption.m_hasChoice)
+            {
+                // put out options
+            }
+            else
+            {
+                //close everything
+            }
+        }
+        else //check if sentence is done printing out
+        {
+            //start next sentence printing
+            ++m_currSentenceIndex;
+            m_currText = m_currDiaOption.m_dialogue[m_currSentenceIndex];
+            StartCoroutine(PrintText(m_currText.m_text.ToCharArray()));
+
+            //TODO
+            //if guy is high, change some of the chars in the text to make it more warped
+        }
+
+
+        //when option is picked
+        //set currDialogueOption, reset currSentence and currCharIndex
+    }
+
+    IEnumerator PrintText(char[] sentence)
+    {
+        if (m_text == null || sentence == null)
+            yield break;
+
+        m_text.text = null;
+        foreach (char letter in sentence)
+        {
+            m_text.text += letter;
+            ++m_currCharIndex;
+
+            yield return new WaitForSeconds(m_textSpeed);
+        }
+
+        yield return null;
     }
 }
