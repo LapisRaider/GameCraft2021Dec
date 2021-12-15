29d919e93df4792dd11989b86d875217c6a1960e
Gideon Francis <gideonnfrancis999@gmail.com>
2021-12-16 03:19:21 +0800

    Added some functions to game manager

--- LosingIsWinning/Assets/Scripts/GameManager.cs
+++ LosingIsWinning/Assets/Scripts/GameManager.cs
@@ -8,27 +8,77 @@ public class GameManager : SingletonBase<GameManager>
     // Need to play test and change values accordingly
     static int m_SANITY_LOST_PER_CAST = 1;
 
-
-    public GameObject m_player;
+    // How long the sanity will last for one use.
+    public float m_SanityTimer = 45.0f;
+    // Used to keep track of the time
+    public float m_CurrentSanityTimer = 0.0f;    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (PlayerData.Instance.m_isInsane)
+        {
+            m_CurrentSanityTimer -= Time.deltaTime;
+            if (m_CurrentSanityTimer <= 0.0f)
+            {
+                PlayerData.Instance.m_isInsane = false;
+                m_CurrentSanityTimer = 0.0f;
+            }
+        }
         // Testing if saving works
         if (Input.GetKey(KeyCode.V)) { SaveSystem.Instance.SaveTheGame(); }
         if (Input.GetKey(KeyCode.B)) { SaveSystem.Instance.LoadTheGame(); }
         if (Input.GetKey(KeyCode.B)) { SaveSystem.Instance.LoadTheGame(); }
+        if (Input.GetKey(KeyCode.E)) { UseSanityAbility(); }
     }
 
     public void UseSanityAbility()
     {
-        PlayerData.Instance.m_currSanityMeter -= m_SANITY_LOST_PER_CAST;
+        // Check if it isnt enabled
+        if (!PlayerData.Instance.m_isInsane)
+        {
+            // Deduct the sanity meter and set the timer
+            PlayerData.Instance.m_currSanityMeter -= m_SANITY_LOST_PER_CAST;
+            // Check if they lost
+            if (PlayerData.Instance.m_currSanityMeter <= 0)
+            {
+                // If they did
+                //idk what happens 
+            }
+            m_CurrentSanityTimer = m_SanityTimer;
+            PlayerData.Instance.m_isInsane = true;
+        }
+        else
+        {
+            // Im not sure if they use it when its already on, will it reset the timer and drain the sanity
+            // Or will it not be allowed to be used until the timer is up 
+        }
+    }
 
+    public void TakeSanityDamage(int sanityLost)
+    {
+        PlayerData.Instance.m_currSanityMeter -= sanityLost;
+
+        if (PlayerData.Instance.m_currSanityMeter <= 0)
+        {
+            // Lost again
+        }
+    }
+
+    public void GainSanity(int sanityGain)
+    {
+        PlayerData.Instance.m_currSanityMeter += sanityGain;
+
+        // Make sure no overflow.
+        if (PlayerData.Instance.m_currSanityMeter >= PlayerData.Instance.m_maxSanityMeter)
+        {
+            PlayerData.Instance.m_currSanityMeter = PlayerData.Instance.m_maxSanityMeter;
+        }
     }
 }
