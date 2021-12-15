59473d465d7f27d84959cb15ee9e1dfed5bdc52f
Gideon Francis <gideonnfrancis999@gmail.com>
2021-12-16 01:51:29 +0800

    Animation for enemies

---
+++ LosingIsWinning/Assets/Scripts/Enemy/MeleeEnemyAnimController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyAnimController : MonoBehaviour
+{
+    MeleeEnemyController enemyController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyController = GetComponentInParent<MeleeEnemyController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void runAttack()
+    {
+        enemyController.Attack();
+    }
+
+    public void endAttack()
+    {
+        enemyController.EndAttack();
+    }
+}
