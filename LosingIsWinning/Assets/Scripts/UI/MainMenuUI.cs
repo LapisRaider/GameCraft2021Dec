02e6bb30cc16ac323bd97f6cccb7ca8e5a9c9f6c
LapisRaider <angielimenqi7@gmail.com>
2021-12-15 18:07:51 +0800

    - have screen fade and basic menu functionalities

---
+++ LosingIsWinning/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public Animator m_MainMenuAnimator;
+
+    [Header("Scene transition")]
+    public float m_TransitionTime = 1.0f;
+    public string m_startSceneName = "AngieTest";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO::play the sound
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(StartLoadGame());
+    }
+
+    IEnumerator StartLoadGame()
+    {
+        if (m_MainMenuAnimator != null)
+            m_MainMenuAnimator.SetTrigger("FadeIn");
+
+        yield return new WaitForSeconds(m_TransitionTime);
+
+        SceneManager.LoadScene(m_startSceneName);
+    }
+}
