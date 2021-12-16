296c5bdb8025708f2f9ec53a8fa578ee7c4efb0a
Gideon Francis <46663395+Gideonnf@users.noreply.github.com>
2021-12-16 23:41:42 +0800

    i hate enemies :weary: :cry: :fire: :fire: :fire:

--- LosingIsWinning/Assets/Scripts/Enemy/MeleeEnemyController.cs
+++ LosingIsWinning/Assets/Scripts/Enemy/MeleeEnemyController.cs
@@ -42,6 +42,7 @@ public class MeleeEnemyController : MonoBehaviour
     public float m_patrolTime;
     [Header("Player Detection")]
     public float m_detectionRange;
+    public float m_attackRange;
 
     bool m_movingRight = true;
     bool m_moving;
@@ -123,16 +124,25 @@ public class MeleeEnemyController : MonoBehaviour
                         // Toggle the moving bool
                         m_moving = !m_moving;
                     }
-
+                    CheckForPlayer();
                     IdleMovement();
                 }
                 break;
             case MELEE_STATES.STATE_MORPHED_CHASE:
                 {
-
+                    CheckForPlayer();
+                    ChasingMovement();
                 }
                 break;
             case MELEE_STATES.STATE_MORPHED_ATTACKING:
+                m_attackTimer += Time.deltaTime;
+
+                if (m_attackTimer >= m_attackTime)
+                {
+                    m_attackTimer = 0;
+                    m_morphedGO.GetComponent<Animator>().SetBool("Attack", true);
+                }
+
                 break;
             default:
                 break;
@@ -145,34 +155,43 @@ public class MeleeEnemyController : MonoBehaviour
         m_attackGO.transform.position = transform.position;
         m_attackGO.SetActive(true);
         m_attackGO.GetComponent<MeleeEnemyAttack>().startAttack = true;
-        m_currState = MELEE_STATES.STATE_MORPHED_IDLE;
+        //m_currState = MELEE_STATES.STATE_MORPHED_CHASE;
     }
 
     public void EndAttack()
     {
-        m_currState = MELEE_STATES.STATE_MORPHED_IDLE;
+        m_currState = MELEE_STATES.STATE_MORPHED_CHASE;
         m_morphedGO.GetComponent<Animator>().SetBool("Attack", false);
+        m_attackTimer = 0;
     }
 
     public void CheckForPlayer()
     {
-        RaycastHit2D hitInfoRight = Physics2D.Raycast(transform.position, Vector2.right, m_detectionRange);
-        RaycastHit2D hitInfoLeft = Physics2D.Raycast(transform.position, Vector2.left, m_detectionRange);
+        RaycastHit2D hitInfoRight = Physics2D.Raycast(transform.position, Vector2.right, m_detectionRange, ~gameObject.layer);
+        RaycastHit2D hitInfoLeft = Physics2D.Raycast(transform.position, Vector2.left, m_detectionRange, ~gameObject.layer);
 
+        Debug.DrawRay(transform.position, (Vector2.right * m_detectionRange), Color.red);
+        Debug.DrawRay(transform.position, (Vector2.left * m_detectionRange), Color.red);
         if (hitInfoRight.collider == true)
         {
+            Debug.Log(hitInfoRight.collider.gameObject.name);
+
             if (hitInfoRight.collider.gameObject.tag == "Player")
             {
                 m_currState = MELEE_STATES.STATE_MORPHED_CHASE;
-    
+                m_patrolTimer = 0.0f;
+                RotateRight();
             }
         }
         else if (hitInfoLeft.collider == true)
         {
+            Debug.Log(hitInfoLeft.collider.gameObject.name);
+
             if (hitInfoLeft.collider.gameObject.tag == "Player")
             {
                 m_currState = MELEE_STATES.STATE_MORPHED_CHASE;
                 m_patrolTimer = 0.0f;
+                RotateLeft();
             }
         }
     }
@@ -190,12 +209,14 @@ public class MeleeEnemyController : MonoBehaviour
             if (m_movingRight)
             {
                 hitInfoForward = Physics2D.Raycast(m_groundDetection.position, Vector2.right, m_distance);
-                Debug.DrawRay(m_groundDetection.position, (Vector2.right * m_distance), Color.green);
+                //Debug.DrawRay(m_groundDetection.position, (Vector2.right * m_distance), Color.green);
+
             }
             else
             {
                 hitInfoForward = Physics2D.Raycast(m_groundDetection.position, Vector2.left, m_distance);
-                Debug.DrawRay(m_groundDetection.position, (Vector2.left * m_distance), Color.green);
+
+                //Debug.DrawRay(m_groundDetection.position, (Vector2.left * m_distance), Color.green);
             }
             // Down collider did not hit anything
             // If forward collider hit something, it means there is smth infront of it
@@ -227,6 +248,81 @@ public class MeleeEnemyController : MonoBehaviour
 
     public void ChasingMovement()
     {
+
+        RaycastHit2D hitInfoRight = Physics2D.Raycast(transform.position, Vector2.right, m_attackRange, ~gameObject.layer);
+        RaycastHit2D hitInfoLeft = Physics2D.Raycast(transform.position, Vector2.left, m_attackRange, ~gameObject.layer);
+      
+        if (hitInfoRight.collider == true && hitInfoRight.collider.gameObject.tag == "Player")
+        {
+            // if they are facing left
+            if (m_movingRight == false)
+            {
+                RotateRight();
+            }
+
+            m_currState = MELEE_STATES.STATE_MORPHED_ATTACKING;
+            m_morphedGO.GetComponent<Animator>().SetBool("Attack", true);
+            m_patrolTimer = 0.0f;
+            m_moving = false;
+        }
+        else if (hitInfoLeft.collider == true && hitInfoLeft.collider.gameObject.tag == "Player")
+        {
+            // If they facing left
+            if (m_movingRight == true)
+            {
+                RotateLeft();
+            }
+
+            m_currState = MELEE_STATES.STATE_MORPHED_ATTACKING;
+            m_morphedGO.GetComponent<Animator>().SetBool("Attack", true);
+            m_patrolTimer = 0.0f;
+            m_moving = false;
+        }
+        else
+        {
+            // no player in range
+            m_moving = true;
+        }
+
+        // else just move towards the player
+        if (m_moving)
+        {
+            transform.Translate(Vector2.right * m_speed * Time.deltaTime);
+
+            // Get the bottom and right hit info 
+            // Checks if theres a wall infront or a drop below
+            RaycastHit2D hitInfoDown = Physics2D.Raycast(m_groundDetection.position, Vector2.down, m_distance);
+            RaycastHit2D hitInfoForward;
+            if (m_movingRight)
+            {
+                hitInfoForward = Physics2D.Raycast(m_groundDetection.position, Vector2.right, m_distance);
+                //Debug.DrawRay(m_groundDetection.position, (Vector2.right * m_distance), Color.green);
+
+            }
+            else
+            {
+                hitInfoForward = Physics2D.Raycast(m_groundDetection.position, Vector2.left, m_distance);
+
+                //Debug.DrawRay(m_groundDetection.position, (Vector2.left * m_distance), Color.green);
+            }
+            // Down collider did not hit anything
+            // If forward collider hit something, it means there is smth infront of it
+            // If it did hit then it means the player ran
+            if (hitInfoForward.collider == true && hitInfoForward.collider.gameObject.tag == "Map")
+            {
+                m_currState = MELEE_STATES.STATE_MORPHED_IDLE;
+                m_patrolTimer = 0.0f;
+                m_moving = false;
+
+            }
+            else if (hitInfoDown.collider == false)
+            {
+                m_currState = MELEE_STATES.STATE_MORPHED_IDLE;
+                m_patrolTimer = 0.0f;
+                m_moving = false;
+
+            }
+        }
 
     }
 
