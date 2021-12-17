006eeb9aaba4ad00797df8f3835824fbcf40db02
LapisRaider <angielimenqi7@gmail.com>
2021-12-17 21:49:24 +0800

    - slash effect for boss

--- LosingIsWinning/Assets/Scripts/Effects/ReplayEffects.cs
+++ LosingIsWinning/Assets/Scripts/Effects/ReplayEffects.cs
@@ -2,6 +2,8 @@
 
 public class ReplayEffects : MonoBehaviour
 {
+    public string m_stateName = "effects";
+
     Animator m_animator;
 
     // Start is called before the first frame update
@@ -12,7 +14,7 @@ public class ReplayEffects : MonoBehaviour
 
     private void OnEnable()
     {
-        m_animator.Play("effects", -1, 0.0f);
+        m_animator.Play(m_stateName, -1, 0.0f);
     }
 
     public void FinishAnim()
