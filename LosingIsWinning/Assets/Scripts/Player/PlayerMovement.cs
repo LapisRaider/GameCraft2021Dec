4a70e4944e169770897519f8b7d88db4b3da294e
LapisRaider <angielimenqi7@gmail.com>
2021-12-18 09:53:02 +0800

    put some sounds in playermovement

--- LosingIsWinning/Assets/Scripts/Player/PlayerMovement.cs
+++ LosingIsWinning/Assets/Scripts/Player/PlayerMovement.cs
@@ -140,7 +140,12 @@ public class PlayerMovement : MonoBehaviour
             
         m_inputDir.x = Input.GetAxisRaw("Horizontal");
         m_inputDir.y = Input.GetAxisRaw("Vertical");
- 
+
+        if (m_inputDir.x != 0.0f && m_isGrounded)
+            SoundManager.Instance.Play("Running");
+        else
+            SoundManager.Instance.Stop("Running");
+
         //update for animation
         if (m_inputDir.x != 0.0f && !m_isDashing)
         {
@@ -174,6 +179,7 @@ public class PlayerMovement : MonoBehaviour
             --m_currJumps;
             m_startJump = true;
             ParticleEffectObjectPooler.Instance.PlayParticle(m_groundCheckPos.position, PARTICLE_EFFECT_TYPE.JUMP);
+            SoundManager.Instance.Play("Jump");
         }
 
         //for dashing
@@ -198,6 +204,7 @@ public class PlayerMovement : MonoBehaviour
 
                         m_rigidBody.gravityScale = 0.0f;
                         ParticleEffectObjectPooler.Instance.PlayParticle(transform.position, PARTICLE_EFFECT_TYPE.DASH);
+                        SoundManager.Instance.Play("Dash");
                     }
                 }
             }
