115aafa2a6394d2ac1a3b92f75539ff442e3396f
Gideon Francis <46663395+Gideonnf@users.noreply.github.com>
2021-12-20 23:24:24 +0800

    Removed the old project

--- LosingIsWinning/Assets/Scripts/Dialogue/DialogueData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class DialogueData : ScriptableObject
-{
-    [Header("Dialogue")]
-    public List<Dialogue> m_Dialogues = new List<Dialogue>();
-}
